92ebe5bd44f7777c2b4e4450a74b1b662c4d5de4
Loki Lust <support@domainsofcanada.ca>
2021-10-23 22:47:46 -0400

    Update createServerPair.sh

--- createServerPair.sh
+++ createServerPair.sh
@@ -8,16 +8,19 @@ fi
 CN="$1"
 
 # generate server key
+cd /root/ca
 openssl genrsa -aes256 \
       -out intermediate/private/${CN}.key.pem 2048
 chmod 400 intermediate/private/${CN}.key.pem
 
 # generate server CSR
+cd /root/ca
 openssl req -config intermediate/openssl.cnf \
       -key intermediate/private/${CN}.key.pem \
       -new -sha256 -out intermediate/csr/${CN}.csr.pem
 
 # sign server CSR with intermediate CA key
+cd /root/ca
 openssl ca -config intermediate/openssl.cnf \
       -extensions server_cert -days 375 -notext -md sha256 \
       -in intermediate/csr/${CN}.csr.pem \
@@ -25,10 +28,12 @@ openssl ca -config intermediate/openssl.cnf \
 chmod 444 intermediate/certs/${CN}.cert.pem
 
 # view server certificate
+cd /root/ca
 openssl x509 -noout -text \
       -in intermediate/certs/${CN}.cert.pem
 
 # verify server certificate
+cd /root/ca
 openssl verify -CAfile intermediate/certs/ca-chain.cert.pem \
       intermediate/certs/${CN}.cert.pem
 
